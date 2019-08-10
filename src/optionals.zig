6fc9d1c9a7c79a1548c49e1aa8ca618ef23123c2
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2019-08-10 22:48:28 +0200

    Improve the optionals example

--- src/optionals.zig
+++ src/optionals.zig
@@ -6,8 +6,18 @@ pub fn main() void {
 }
 
 pub fn nulledStringExample() void {
-    var optional_string: ?[]const u8 = null;
+    var optional_string_null: ?[]const u8 = null;
+    var optional_string_non_null: ?[]const u8 = "non-null";
 
     warn("\nExamples with optional values.\n");
-    warn("Nulled string: {}\n", optional_string);
+    detectNull(optional_string_null);
+    detectNull(optional_string_non_null);
+}
+
+pub fn detectNull(optional_value: ?[]const u8) void {
+    if (optional_value) |value| {
+        warn("value is guaranteed not null in this branch, is: {}\n", value);
+    } else {
+        warn("optional_value is guaranteed null in this branch, is: {}\n", optional_value);
+    }
 }
