6e4195a2922407dc2337c8480a9bd170591b3c6e
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2019-08-10 16:59:11 +0200

    Add an example of packed structs

--- OBSERVATIONS.md
+++ OBSERVATIONS.md
@@ -30,9 +30,10 @@ Questions:
 * How to import another file
     * Tried stuff until it worked
 
-## Things that were hard to figure out
+## Things I still haven't figured out
 
 * How to iterate over imported modules (wanted to iterate over them in `all.zig` and call their `main()` functions, got something about type having to be comptime).
 * How to get C interop to work on my machine (something wrong with my setup? Enormous amounts of errors from `ldd`)
 * How to compile C files on my machine (something wrong with my setup? Enormous amounts of errors from `ldd`)
 * In threads.zig, when calculating the next thread ID, it would be convenient with a static variable (either associated with the struct or from inside the init() function, like static in C). Can that be done?
+* Can I create a struct within a function? It fails when it tries to refer to its own type ("undeclared identifier").
--- src/structs.zig
+++ src/structs.zig
@@ -1,25 +1,52 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const warn = std.debug.warn;
 
-const ExampleStruct = struct {
+const StructExample = struct {
     x: i32,
     y: i32,
 
-    pub fn zero() ExampleStruct {
-        return ExampleStruct{
+    pub fn zero() StructExample {
+        return StructExample{
             .x = 0,
             .y = 0,
         };
     }
 };
 
+const PackedStructExample = packed struct {
+    five_bits: u5,
+    two_bits: u2,
+    one_bit: u1,
+
+    pub fn from_byte(byte: u8) PackedStructExample {
+        // This seems platform dependent.
+        return PackedStructExample{
+            .one_bit = @intCast(u1, (byte & 0b10000000) >> 7),
+            .two_bits = @intCast(u2, (byte & 0b01100000) >> 5),
+            .five_bits = @intCast(u5, (byte & 0b00011111)),
+        };
+    }
+};
+
 pub fn main() void {
     structCreationExample();
+    packedStructExample();
 }
 
 pub fn structCreationExample() void {
-    const zero: ExampleStruct = ExampleStruct.zero();
+    const zero: StructExample = StructExample.zero();
 
     warn("\nExample of struct creation.\n");
-    warn("Zeroed ExampleStruct: {}\n", zero);
+    warn("Zeroed StructExample: {}\n", zero);
+}
+
+pub fn packedStructExample() void {
+    const bitfield_value: u8 = 0b10101011;
+    const bitfield: PackedStructExample = PackedStructExample.from_byte(bitfield_value);
+
+    warn("\nExample of using a packed struct.\n");
+    warn("Byte: {b}\n", @intCast(u8, bitfield_value)); // Cast due to compiler bug
+    warn("Resulting bitfield: {}\n", bitfield);
+    warn("Bits of the bitfield after bit-casting to byet: {b}\n", @bitCast(u8, bitfield));
 }
